303954b8b345905d3770cebe7dc4975cb690ae2a
spainisnotequal <spainisnotequal@gmail.com>
2020-04-09 21:17:07 +0200

    Add the timer functionality

--- functions.js
+++ functions.js
@@ -1,29 +1,31 @@
-function start() {
-  const timestampList = document.getElementById("timestamp-list");
-  const element = document.createElement("li");
-  element.textContent = "0.0000";
-  element.className = "list-group-item";
-  timestampList.appendChild(element);
+let START_TIME = undefined;
 
-  document.getElementById("start").disabled = true;
-  document.getElementById("timestamp").disabled = false;
-  document.getElementById("stop").disabled = false;
-}
-
-const timestamp = () => {
-  const timestampList = document.getElementById("timestamp-list");
+const createLiChild = (parent, textContent) => {
+  const timestampList = document.getElementById(parent);
   const element = document.createElement("li");
-  element.textContent = "timestamp";
+  element.textContent = textContent;
   element.className = "list-group-item";
   timestampList.appendChild(element);
 };
 
+const start = () => {
+  createLiChild("timestamp-list", "0.000");
+  START_TIME = Date.now();
+
+  document.getElementById("start").disabled = true;
+  document.getElementById("timestamp").disabled = false;
+  document.getElementById("stop").disabled = false;
+};
+
+const timestamp = () => {
+  createLiChild("timestamp-list", (Date.now() - START_TIME) / 1000);
+};
+
 const stop = () => {
   const timestampList = document.getElementById("timestamp-list");
-  const element = document.createElement("li");
-  element.textContent = "stop";
-  element.className = "list-group-item";
-  timestampList.appendChild(element);
+  while (timestampList.firstChild) {
+    timestampList.removeChild(timestampList.firstChild);
+  }
 
   document.getElementById("start").disabled = false;
   document.getElementById("timestamp").disabled = true;
